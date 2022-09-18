89c48cd6d2f1129045afdb83c1b514af9172b795
David Torres <luis.torres.o@utec.edu.pe>
2022-09-17 19:30:24 -0500

    Add a new cout in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main() {
     std::cout << "Holaaa! Este es mi primer proyecto con Clion y Git" << std::endl;
+    cout << "Nuevo cambio added!!" << endl;
 
     return 0;
 }

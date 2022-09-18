dec42530365cc8e76f78a9e0afa8825e558f53a9
Luis-David Torres <63759366+davidt02tech@users.noreply.github.com>
2022-09-17 19:31:56 -0500

    Update main.cpp
    Add a comment to main from GitHub

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 int main() {
+    //// ESTOY ADDING CAMBIOS FROM GITHUB
     std::cout << "Holaaa! Este es mi primer proyecto con Clion y Git" << std::endl;
     cout << "Nuevo cambio added!!" << endl;
+   
 
     return 0;
 }
